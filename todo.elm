9499a99166d4ee0ffae89794a309d5876030488a
k <moskala.k@gmail.com>
2018-03-04 11:04:18 +0100

    initial commit

---
+++ todo.elm
@@ -0,0 +1,56 @@
+import Html exposing (..)
+import Html.Events exposing (..)
+
+main = Html.beginnerProgram {model = model, view = view, update = update}
+
+type alias User = {
+    firstName : String
+    , lastName : String
+}
+user: User
+user = {firstName = "Kacper", lastName = "Moskala"}
+
+fullname: User -> String
+fullname user = user.firstName ++ " " ++ user.lastName
+
+type Msg = 
+    ShowForm
+
+type Mode = 
+    AddNode | ShowButton
+
+type alias Todo = {
+    id : Int
+    , content : String
+}
+
+type alias Model = {
+    user : User
+    , todos : List Todo
+    , mode : Mode
+}
+model: Model
+model = {
+    user = user
+    , todos = [
+        Todo 1 "write app"
+        , Todo 2 "write app again" ]
+    , mode = ShowButton }
+
+view: Model -> Html Msg
+view model = 
+    Html.div [] [
+        h1 [] [text (fullname model.user)]
+        , ul [] (List.map (\ todo -> Html.li [] [text todo.content]) model.todos)
+        , handleForm model.mode
+    ]
+
+handleForm mode =
+    case mode of
+        ShowButton -> button [ onClick ShowForm ] [text "Add note" ]
+        AddNode -> input [] []
+
+update: Msg -> Model -> Model
+update msg model
+    = case msg of
+        ShowForm -> {model | mode = AddNode}
